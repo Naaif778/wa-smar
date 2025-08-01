7a43c6307ba3f5ac2a34f6a7bb0fef7fe1ca85c6
Naif alharbi <n4334@Naifs-MacBook-Air.local>
2025-08-02 01:51:42 +0300

    add main page to app Router

---
+++ smar-wa.com/app/page.tsx
@@ -0,0 +1,10 @@
+// smar-wa.com/pages/index.tsx
+export default function Home() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h1>Welcome to Smar-WA 👋</h1>
+        <p>Your Next.js project is live on Vercel 🚀</p>
+      </div>
+    );
+  }
+  
